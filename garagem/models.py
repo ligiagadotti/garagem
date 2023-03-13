b52399565a8906f46e9316c99dd5115d61c5e8ad
ligia <angelisgadotti@gmail.com>
2023-03-13 16:50:02 -0300

    Alteração 6 (migrations)

--- garagem/models.py
+++ garagem/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50, blank=True)
+    nacionalidade = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.nome.upper()
