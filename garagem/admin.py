ff90c17b32fae580e4fec36924de42bd23e9af33
ligia <angelisgadotti@gmail.com>
2023-03-31 10:30:11 -0300

    Alteração 7 (passo 6.8)

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from garagem.models import Categoria, Marca
+from garagem.models import Categoria, Marca, Veiculo, Acessorio, Cor
 
-admin.site.register(Marca)
 admin.site.register(Categoria)
-
+admin.site.register(Marca)
+admin.site.register(Veiculo)
+admin.site.register(Acessorio)
+admin.site.register(Cor)
--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from garagem.models import Categoria, Marca, Veiculo, Acessorio, Cor
+from garagem.serializers import CategoriaSerializer, MarcaSerializer, VeiculoSerializer, VeiculoDetailSerializer, AcessorioSerializer, CorSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return VeiculoDetailSerializer
+        return VeiculoSerializer
