996125303681c701943edc4e97656e84eb92431b
ligia <angelisgadotti@gmail.com>
2023-08-07 16:33:42 -0300

    Passo 11 e 12 (grupos e permissões)

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ]
+}
